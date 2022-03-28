87f3806ce71613af0ccb1355b7dde6f4f4a876f7
Shaan Wocker <shaanwocker@gmail.com>
2022-03-28 14:00:05 +0200

    Routes file with CRUD routes

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+// GET
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'Get goals' });
+});
+
+// POST
+router.post('/', (req, res) => {
+  res.status(200).json({ message: 'Set goals' });
+});
+
+// PUT
+router.put('/:id', (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+});
+
+// DELETE
+router.delete('/:id', (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+});
+
+module.exports = router;
