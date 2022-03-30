021902789b878146b639d3684b0ac73fe58347b8
Shaan Wocker <shaanwocker@gmail.com>
2022-03-30 14:05:59 +0200

    Auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,12 +5,13 @@ const {
   loginUser,
   getMe,
 } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 
 // POST
 router.post('/', registerUser);
 router.post('/login', loginUser);
 
 // GET - USER DATA
-router.get('/me', getMe);
+router.get('/me', protect, getMe);
 
 module.exports = router;
